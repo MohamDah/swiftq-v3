5e5613fdbc7731b8cc4aad8b84570e134254cc35
MohamDah <mohammedfaiseldhb@gmail.com>
2026-02-17 17:34:28 +0200

    feat: refactor routing structure by moving routes to mainRoutes file

--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./components/ErrorPage";
-import Layout from "./components/Layout";
-import CustomerLayout from "./components/CustomerLayout";
-import CreateQueue from "./pages/CreateQueue";
-import Home from "./pages/Home";
-import JoinQueue from "./pages/join-queue/JoinQueue";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import CustomerView from "./pages/CustomerView";
-import QR from "./pages/QR";
 import { QueryProvider } from "./providers/QueryProvider";
-import ProtectedRoute from "./components/ProtectedRoute";
-import HostQueues from "./pages/host-queues/HostQueues";
-import HostQueueDetails from "./pages/HostQueueDetails";
+import mainRoutes from "./routes/mainRoutes";
 
+const router = createBrowserRouter(mainRoutes);
 
-const router = createBrowserRouter([
-  {
-    errorElement: <ErrorPage />,
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      //     // Host Views
-      {
-        element: <CustomerLayout />,
-        children: [
-          {
-            path: "/queue/:qrCode/customer",
-            element: (
-              <CustomerView />
-            ),
-          },
-          {
-            path: "/join/:qrCode",
-            element: (
-              <JoinQueue />
-            ),
-          },
-          {
-            path: "/create",
-            element: (
-              <ProtectedRoute>
-                <CreateQueue />
-              </ProtectedRoute>
-            )
-          }
-        ]
-      },
-      {
-        path: "/my-queues",
-        element: (
-          <ProtectedRoute>
-            <HostQueues />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/my-queues/:queueId",
-        element: (
-          <ProtectedRoute>
-            <HostQueueDetails />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/qr/:queueId",
-        element: (
-          <ProtectedRoute>
-            <QR />
-          </ProtectedRoute>
-        ),
-      },
-      //     {
-      //       path: "/analytics",
-      //       element: (
-      //         <ProtectedRoute>
-      //           <Analytics />
-      //         </ProtectedRoute>
-      //       ),
-      //     },
-
-
-      //     // Auth Views
-      {
-        path: "/login",
-        element: <Login />
-      },
-      {
-        path: "/register",
-        element:
-          <Signup />
-      },
-    ]
-  }
-]);
 function App() {
-
-
   return (
     <QueryProvider>
       <RouterProvider router={router} />
